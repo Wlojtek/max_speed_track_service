0872e948aac946fa7906ce2f23d274996c48fa65
Wlojtek <wojciech.lazar@gmail.com>
2017-08-28 23:54:13 +0200

    added specs

--- spec/controllers/api/v1/cars_controller_spec.rb
+++ spec/controllers/api/v1/cars_controller_spec.rb
@@ -1,8 +1,10 @@
+require 'rails_helper'
+
 describe Api::V1::CarsController do
   describe '#show' do
     it 'returns cars details' do 
       get :show, car_slug: :subar_impreza
-      expect(response.body).to contains(:car_slug)
+      expect(response.body).to include('car_slug')
     end 
   end
 end
