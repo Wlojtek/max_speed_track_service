b2ea5b37ffd9865d40ea6e7e9a2b55cf2ad5f7d7
Wlojtek <wojciech.lazar@gmail.com>
2017-08-29 22:35:13 +0200

    expose endpoint, add presenters

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::CarsController < ApiController
+  before_filter :find_car, :find_track
+
   def show
-    render json: { 
-      car: { car_slug: params[:car_slug] }
-    }
+    @max_speed_entry = MaxSpeedEntry.new(car: @car, track: @track)
+    max_speed_on_track = MaxSpeedOnTrackService.new(entry: max_speed_entry, strategy: SimpleMaxSpeedStrategy.new).get
+
+    render json: { car: CarPresenter.new(car: @car, max_speed_on_track: max_speed_on_track).presentable }
+  end
+
+  def find_car
+    @car = Car.find(car_slug: params[:car_slug])
+  end
+
+  def find_track
+    @track = Track.find(name: params[:track])
   end
 end
---
+++ app/presenters/car_presenter.rb
@@ -0,0 +1,19 @@
+class CarPresenter
+  def initialize(car:, max_speed_on_track)
+    @car = car
+    @max_speed_on_track = max_speed_on_track
+  end
+
+  def presentable
+    {
+      id: @car.id,
+      car_slug: @car.car_slug,
+      max_speed: "#{@car.max_speed} km/h",
+      max_speed_on_track: @max_speed_on_track
+    }
+  end
+
+  def as_json(options = {})
+    presentable.to_json
+  end
+end
